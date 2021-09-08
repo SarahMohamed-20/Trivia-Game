c741688397a20f5d8632feae26c49424d1e3ec44
SarahMohamed-20 <88808030+SarahMohamed-20@users.noreply.github.com>
2021-09-08 19:21:11 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+int main()
+{string q[41];int ans[41];
+cout<<"WELCOME TO THE QUESTIONS GAME\nEVERY TIME YOU GET THE ANSWER RIGHT, YOU WILL GET 10 POINTS\nIF YOU GET THE ANSWER WRONG, YOU CAN EITHER [START OVER] OR [QUIT]\n                 NOW,LET`S START\n";
+q[0]="For which of the following disciplines is Nobel Prize awarded?\n1.Literature, Peace and Economics\n2.Physics and Chemistry\n3.Physiology or Medicine\n4.All of the above";
+ans[0]=4;
+q[1]="Galileo was an astronomer who;\n1.developed the telescope\n2.discovered four satellites of Jupiter\n3.discovered that the movement of pendulum produces a regular time measurement\n4.All the above.";
+ans[1]=2;
+q[2]="Entomology is the science that studies:\n1.Behaviour of human beings\n2.Insects\n3.The origin and history of technical and scientific terms\n4.The formation of rocks";
+ans[2]=2;
+q[3]="Name the currency used in Japan:\n1.Taka\n2.Dinar\n3.Ngultrum\n4.Yen\n";
+ans[3]=4;
+q[4]="In which country is the Leaning Tower of Pisa located:\n1.England\n2.Spain\n3.France\n4.Italy\n";
+ans[4]=4;
+q[5]="A xylophone is:\n1.A tool that measures seismic waves\n2.A musical instrument\n3.A stereo telephone\n4.A medical device that measures heart rate\n";
+ans[5]=2;
+q[6]="How many dots are on one six-sided die?\n1.17\n2.19\n3.20\n4.21\n";
+ans[6]=4;
+q[7]="World War II ended in\n1.1942\n2.1943\n3.1945\n4.1947\n";
+ans[7]=3;
+q[8]="Paleontologists study:\n1.Mountains\n2.Animals\n3.Lost civilizations\n4.Fossils\n";
+ans[8]=4;
+q[9]="Ottawa is the capital of:\n1.Bolivia\n2.India\n3.Canada\n4.Australia\n";
+ans[9]=3;
+q[10]="A supernova is a:\n1.An underwater volcano\n2.An intense lightning storm\n3.The explosion of a star\n4.The eye of a tornado\n";
+ans[10]=3;
+q[11]="How are bats able to fly in the dark?\n1.X-ray vision\n2.Sixth sense\n3.Echolocation\n4.Inter-species communication\n";
+ans[11]=3;
+q[12]="The technical term for a lie detector is:\n1.Polygraph\n2.Seismograph\n3.Teragraph\n4.Omnigraph\n";
+ans[12]=1;
+q[13]="The smallest country in the world is:\n1.Monaco\n2.Liechtenstein\n3.Vatican City\n4.San Marinon\n";
+ans[13]=3;
+q[14]="Mount Everest is located in:\n1.Tibet\n2.Nepal\n3.On the border between Tibet and Nepal\n4.Switzerland\n";
+ans[14]=3;
+q[15]="What number is on Herbie the beatle?\n1.45\n2.53\n3.7\n4.89";
+ans[15]=2;
+q[16]="In which Nintendo DS game do you have to raise a puppy as well as possible?\n1.Nintendoanimals\n2.Nintendogs\n3.Nintendinausors\n4.Nintendocats";
+ans[16]=2;
+q[17]="What does USB stand for in the computer world?\n1.Universal serious bus\n2.University serial Bus\n3.Universal Serial Bus\n4.Universal serial bag";
+ans[17]=3;
+q[18]="What is the name of the German airline?\nGerman Airways\n2.Lufthansa\n3.Deutsch Airways\n4.EgyptAir ";
+ans[18]=2;
+q[19]="In which European city can you find the home of Anne Frank?\n1.Cairo\n2.Berlin\n3.Paris\n4.Amsterdam";
+ans[19]=4;
+q[20]="How many stars has the American flag got?\n1.34\n2.55\n3.50\n4.60";
+ans[20]=3;
+q[21]="How long is the Great Wall of China?\n1.4000 miles\n2.50000 miles\n3.3000 miles\n4.2500 miles";
+ans[21]=1;
+q[22]="Who invented Ferrari?\n1.Enzo Patrick\n2.Ralph Laurent\n3.Enzo Ferrari\n4.Jean Fred";
+ans[22]=3;
+q[23]="Which two months are named after Roman emperors?\n1.December and November\n2.September and August\n3.July and March\n4.July and August";
+ans[23]=4;
+q[24]="What is the largest number of five digits?\n1.99999\n2.55555\n3.89999\n4.11111";
+ans[24]=1;
+q[25]="What is the most fractured human bone?\n1.Fumer\n2.Scapula\n3.Clavicle\n4.Humerus";
+ans[25]=3;
+q[26]="What is the most famous university of Paris?\n1.Henry 4\n2.Sorbonne\n3.Paris Sud\n4.Paris Diderot";
+ans[26]=2;
+q[27]="What is the name of the Indian holy river?\n1.Seine\n2.Thames\n3.Nile\n4.Ganges";
+ans[27]=4;
+q[28]="How many stars feature on the flag of New Zealand?\n1.5\n2.6\n3.4\n4.7";
+ans[28]=3;
+q[29]="What color to do you get when you mix red and white?\n1.Pink\n2.Green\n3.Blue\n4.0range";
+ans[29]=1;
+q[30]="What is both a French wine region and a luxury American automobile?\n1.Renault\n2.Cadillac\n3.Audi\n4.BMW";
+ans[30]=2;
+q[31]="Which South American country is named after Venice?\n1.Brazil\n2.Uruguay\n.3.Chile\n4.Venezuela ";
+ans[31]=4;
+q[32]="Which animal is on the golden Flemish flag?\n1.Tiger\n2.Lizard\n3.Lion\n4.Giraffe";
+ans[32]=3;
+q[33]="Who is the director of the X-files?\n1.Rob Bowman\n2.Steven Spielberg\n3.Martin Scorsese\n4.Stanly Kubrick";
+ans[33]=1;
+q[34]="Who did play the role of Peter Pan in the Peter Pan movie?\n1.Eddie Murphy\n2.Will Smith\n3,Samuel Lee Jackson\n4.Robin Williams";
+ans[34]=4;
+q[35]="What is the name of the bald commander of the Enterprise in Star Trek?\n1.Captain Fincard\n2.Captain Picard\n3.Captain Backyard\n4.Captain Lakeyard";
+ans[35]=2;
+q[36]="What is the name of Steven Spielberg s black-and-white-film about the second world war?\n1.Schindler's List\n2.Dunkirk\n3.Inglorious Bastards\n4.Valkyrie";
+ans[36]=1;
+q[37]="Who played the lead role in the movie Scarface in 1983?\n1.Tom Cruise\n2.AlPacino\n3.Matt Damon\n4.Leonardo Decaprio";
+ans[37]=2;
+q[38]="Who played Che Guevara in the movie Evita?\n1.Brad Pitt\n2.Chris Pratt\n3.Harrison Ford\n4.Antonio Banderas";
+ans[38]=4;
+q[39]="How many oscars did the Titanic movie got?\n1.4\n2.7\n3.11\n4.9";
+ans[39]=3;
+q[40]="What is the largest vegetable?\n1.Cucumber\n2.Pumpkin\n3.Pepper\n4.Tomato";
+ans[40]=2;
+int n=1,x,answers,a;
+while(n!=0){
+    int points=0,count=0;
+    x=1;
+    while(x!=0){
+  int ran=rand()%41;
+        cout<<q[ran]<<endl;
+    count++;
+    cin>>answers;
+
+    if(answers==ans[ran]){
+        points+=10;
+       cout<<"                                                      score= "<<points<<endl;}
+      else{
+
+       cout<<"Wrong Answer\nYour total score is "<<points<<endl;
+
+        break;}
+         if(count==15)
+        break;
+    }
+
+
+cout<<"0 to exit 1 to restart";
+cin>>a;
+if(a==0)
+    break;
+else if(a==1)
+    continue;
+}
+    return 0;
+}
